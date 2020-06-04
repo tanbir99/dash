132c0dfa0aa8b874b34e4a15186818480edb575c
tanbir99 <tanbir.islam@baruchmail.cuny.edu>
2020-06-03 21:53:54 -0400

    convert times to milliseconds

--- js/popup.js
+++ js/popup.js
@@ -31,4 +31,51 @@ function setToday(s, v) {
     }
 }
 
-setToday(document.getElementById('theDay'), day);
+var mondaySched = [
+    { class1: 'STA 3155',
+      hour: 10,
+      minutes: 45,
+      ampm: "AM"
+    },
+    { class2: 'CIS 3400',
+      hour: 4,
+      minutes: 10,
+      ampm: "PM"
+    },
+    { class3: 'POL 3362',
+      hour: 5,
+      minutes: 50,
+      ampm: "PM"
+    },
+  ];
+
+var monday = new Array()
+var currentClass = null
+
+
+
+
+function milliMaker(daySched) {
+    for (var i = 0; i < daySched.length; i++) {
+        if (daySched[i].ampm == "AM") {
+            if (daySched[i].hour == 12) {
+                monday.push(daySched[i].minutes * 60000)
+            } else {
+                monday.push(((daySched[i].hour * 60) + daySched[i].minutes) * 60000 ) ;
+            }
+        } else {
+            if (daySched[i].ampm == 12) {
+                monday.push((720 + daySched[i].minutes) * 60000)
+            } else {
+                monday.push((660 + (daySched[i].hour * 60) + daySched[i].minutes) * 60000 ) ; 
+            }
+        }
+        console.log(monday)
+        }
+    }
+
+
+milliMaker(mondaySched)
+
+
+//setToday(document.getElementById('theDay'), day);
