1092ba7401b3e83a04bd060721ab637a765aa16e
tanbir99 <tanbir.islam@baruchmail.cuny.edu>
2020-06-07 19:30:38 -0400

    reorganized code

--- js/popup.js
+++ js/popup.js
@@ -1,56 +1,69 @@
-switch (new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-}
+//Time
+var timeNow = new Date();
+var dayNow = timeNow.getDay();
+var hourNow = timeNow.getHours();
+var minutesNow = timeNow.getMinutes();
+var secondsNow = timeNow.getSeconds();
+
+//Arrays for class times to be stored in milliseconds
+var monday = new Array()
 
 
-function setToday(s, v) {
-    for ( var i = 0; i < s.options.length; i++ ) {
-        if ( s.options[i].text == v ) {
-            s.options[i].selected = true;
+//Preselect today in dropdown menu
+function setToday(days) {
+    for ( var i = 0; i < days.options.length; i++ ) {
+        if ( days.options[i].value == dayNow ) {
+            days.options[i].selected = true;
             return;
         }
     }
 }
 
+setToday(document.getElementById('theDay'));
+
+
+
 var mondaySched = [
-    { class1: 'STA 3155',
+    { class1_end: 'TEST' ,
+      hour: 11,
+      minutes: 59,
+      ampm: 'PM'
+    },
+    { class1_start: 'STA 3155',
       hour: 10,
       minutes: 45,
       ampm: "AM"
     },
-    { class2: 'CIS 3400',
+    { class1_end: 'STA 3155' ,
+      hour: 12,
+      minutes: 00,
+      ampm: 'PM'
+    },
+    { class2_start: 'CIS 3400',
       hour: 4,
       minutes: 10,
-      ampm: "PM"
+      ampm: 'PM'
     },
-    { class3: 'POL 3362',
+    {
+      class2_end: 'CIS 3400',
+      hour: 5,
+      minutes: 25,
+      ampm: 'PM'
+    },
+    { class3_start: 'POL 3362',
       hour: 5,
       minutes: 50,
-      ampm: "PM"
+      ampm: 'PM'
     },
-  ];
+    { class3_end: 'POL 3362',
+      hour: 7,
+      minutes: 05,
+      ampm: 'PM'
+    }
+];
 
-var monday = new Array()
-var currentClass = null
+
+
 
 
 
@@ -64,18 +77,22 @@ function milliMaker(daySched) {
                 monday.push(((daySched[i].hour * 60) + daySched[i].minutes) * 60000 ) ;
             }
         } else {
-            if (daySched[i].ampm == 12) {
+            if (daySched[i].hour == 12) {
                 monday.push((720 + daySched[i].minutes) * 60000)
             } else {
                 monday.push((660 + (daySched[i].hour * 60) + daySched[i].minutes) * 60000 ) ; 
             }
         }
-        console.log(monday)
-        }
     }
+}
 
 
 milliMaker(mondaySched)
 
+console.log(monday)
 
-//setToday(document.getElementById('theDay'), day);
+
+
+
+
+console.log(hourNow, minutesNow, secondsNow)
